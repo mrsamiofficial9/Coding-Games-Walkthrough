566eadadadb60af81cc78ae67ca2d7d7662da461
Samson Tesfaye <124459773+mrsamiofficial9@users.noreply.github.com>
2024-03-16 01:02:17 +0300

    Create php.php

---
+++ [The Descent]/php.php
@@ -0,0 +1,13 @@
+<?php
+  while(TRUE){
+    $h_h = 0;
+    $h_m = 0;
+    for ($i = 0; $i < 8; $i++){
+      fscanf(STDIN, "%d", $m_h);
+     if ($h_h < $m_h){
+       $h_h = $m_h
+       $h_m = $i
+    } 
+  }
+  echo $h_m. "/n"l
+?>
